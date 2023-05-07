cbbd436e6f2a685dee0c61a9ad719542c9ba7d84
Gianluca Langhans <coco.gian07@gmail.com>
2023-05-07 22:50:01 +0200

    scaled components to full page

--- src/components/UserData/Index.tsx
+++ src/components/UserData/Index.tsx
@@ -37,7 +37,6 @@ const Container = styled.section`
 
   border-radius: 1.5rem;
   margin-top: 1.6rem;
-  max-width: 73.3rem;
   box-shadow: 0px 16px 30px -10px rgba(70, 96, 187, 0.198567);
   display: flex;
 
